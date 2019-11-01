ddfd524caf73bac222d465c1466c192d189bb2b8
Evan Ramos <evan@hpccharm.com>
2019-11-01 18:18:52 -0500

    fixup: replace usage of Cmm in cpuTopoHandler

--- src/conv-core/cputopology.C
+++ src/conv-core/cputopology.C
@@ -170,7 +170,7 @@ int CpuTopology::supported = 0;
 namespace CpuTopoDetails {
 
 static nodeTopoMsg *topomsg = NULL;
-static CmmTable hostTable;
+static std::vector<skt_ip_t> hostTable;
 
 CpvStaticDeclare(int, cpuTopoHandlerIdx);
 CpvStaticDeclare(int, cpuTopoRecvHandlerIdx);
@@ -205,11 +205,10 @@ static void cpuTopoHandler(void *m)
 {
   _procInfo *rec;
   hostnameMsg *msg = (hostnameMsg *)m;
-  int tag, tag1, pe;
+  int pe;
 
   if (topomsg == NULL) {
     int i;
-    hostTable = CmmNew();
     topomsg = (nodeTopoMsg *)CmiAlloc(sizeof(nodeTopoMsg)+CmiNumPes()*sizeof(int));
     CmiSetHandler((char *)topomsg, CpvAccess(cpuTopoRecvHandlerIdx));
     topomsg->nodes = (int *)((char*)topomsg + sizeof(nodeTopoMsg));
@@ -228,26 +227,31 @@ static void cpuTopoHandler(void *m)
   skt_print_ip(str, sizeof(str), &msg->ip);
   printf("hostname: %d %s\n", msg->pe, str);
 */
-    tag = *(int*)&proc->ip;
+    skt_ip_t & ip = proc->ip;
     pe = proc->pe;
-    if ((rec = (_procInfo *)CmmProbe(hostTable, 1, &tag, &tag1)) != NULL) {
+    bool newEntry = true;
+    for (const skt_ip_t & entry : hostTable)
+    {
+      if (memcmp(&entry, &ip, sizeof(skt_ip_t)) == 0)
+      {
+        newEntry = false;
+        break;
+      }
     }
-    else {
+    if (newEntry)
+    {
       proc->nodeID = pe;           // we will compact the node ID later
       rec = proc;
-      CmmPut(hostTable, 1, &tag, proc);
+      hostTable.push_back(ip);
     }
     topomsg->nodes[pe] = rec->nodeID;
     rec->rank ++;
   }
 
-  printTopology(CmmEntries(hostTable));
+  printTopology(hostTable.size());
 
     // clean up CmmTable
-  hostnameMsg *tmpm;
-  tag = CmmWildCard;
-  while ((tmpm = (hostnameMsg *)CmmGet(hostTable, 1, &tag, &tag1)));
-  CmmFree(hostTable);
+  hostTable.clear();
   CmiFree(msg);
 
   CmiSyncBroadcastAllAndFree(sizeof(nodeTopoMsg)+CmiNumPes()*sizeof(int), (char *)topomsg);
